37dd594702d10208c7a4029bb8ce901d5d1e9603
Kosuke Inami <kosuke.i.619@gmail.com>
2022-08-08 08:22:08 +1000

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
+// Fonts ----------
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+
+// Packages ----------
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// Navigation ----------
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Import Screen ----------
+import { Homepage } from "./src/screens";
+
+// Create stack navigator ----------
+const Stack = createNativeStackNavigator()
+
 
 export default function App() {
+
+  // Font loading ----------
+  let [fontsLoaded] = useFonts({
+    'Raleway-SemiBold': require('./assets/fonts/Raleway-SemiBold.ttf'),
+    'Raleway-Regular': require('./assets/fonts/Raleway-Regular.ttf'),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* to pass additional props we have to change our Stack.screen component */}
+        <Stack.Screen name="Welcome" options={{
+          headerTitle: "Welcome!",
+        }}>
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/index.js
@@ -0,0 +1,11 @@
+import WelcomeScreen from "./WelcomeScreen";
+import SigninScreen from "./SigninScreen";
+import SignupScreen from "./SignupScreen";
+import HomeScreen from "./HomeScreen";
+import DetailsScreen from "./DetailsScreen";
+
+export { WelcomeScreen }
+export { SigninScreen }
+export { SignupScreen }
+export { HomeScreen }
+export { DetailsScreen }
